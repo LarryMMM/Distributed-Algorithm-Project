838232623dcf4d4e259a0246fcc8d3d5d414c2f8
Haoran Sun <254664427@qq.com>
2018-04-27 14:48:57 +1000

    Add some Node struct
    Add some struct

---
+++ EZShare/src/main/java/EZShare/server/SuperNode.java
@@ -0,0 +1,18 @@
+package EZShare.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class SuperNode {
+    public String Superid;
+    
+    public SuperNodeStruct(String Superid)
+    {
+        this.Superid = Superid;
+    }
+    
+}
+
+
